1317417f4327f6907228fc82af9783405deda0af
tmthywu <jiabaowu07@gmail.com>
2024-11-18 20:41:20 -0500

    finished gui!!

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
-let humanScore = 0;
-let computerScore = 0;
+let humanScoreValue = 0;
+let computerScoreValue = 0;
+let gameOver = false;
+const humanScore = document.querySelector("#humanScore");
+const computerScore = document.querySelector("#computerScore");
+const container = document.querySelector(".container");
+const humanChoice = document.querySelector("#humanChoice");
+const computerChoice = document.querySelector("#computerChoice");
+const roundResult = document.querySelector("#roundResult");
+const gameResult = document.querySelector("#gameResult");
+
+container.addEventListener('click', (event) => {
+    if (!gameOver) {
+        // getting human and computer choices
+        let target = event.target;
+        let roundComputerChoice = getComputerChoice();
+    
+        // play round
+        let roundResultValue = evaluateRound(target.id, roundComputerChoice);
+    
+        // show human and computer choice
+        humanChoice.textContent = "Human choice: " + target.id;
+        computerChoice.textContent = "Computer choice: " + roundComputerChoice;
+    
+        // update human score
+        humanScore.textContent = "Human score: " + humanScoreValue;
+    
+        // update computer score
+        computerScore.textContent = "Computer score: " + computerScoreValue;
+    
+        // show round result
+        roundResult.textContent = roundResultValue;
+
+        // show game result
+        if (humanScoreValue >= 5) {
+            gameResult.textContent = "Human wins!";
+            gameOver = true;
+        } else if (computerScoreValue >= 5) {
+            gameResult.textContent = "Computer wins!";
+            gameOver = true;
+        }
+    }
+});
 
 function getComputerChoice() {
     let randInt = Math.floor(Math.random() * 3);
@@ -12,40 +53,20 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let input = prompt("Enter 'rock', 'paper', or 'scissors'. ");
-    if (input === 'rock' || input === 'paper' || input === 'scissors') {
-        return input;
-    } 
-    // else return undefined
-}
-
-function playRound(humanChoice, computerChoice) {
+function evaluateRound(humanChoice, computerChoice) {
     let hc = humanChoice.toLowerCase();
     let cc = computerChoice.toLowerCase();
 
     if (humanChoice === computerChoice) {
         // it's a tie!
-        console.log("the round's a tie!")
+        return 'tie';
     } else if (humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'paper') {
         // human wins
-        console.log("human wins round!");
-        humanScore++;
+        humanScoreValue++;
+        return 'human';
     } else {
         // computer wins
-        console.log("computer wins round!");
-        computerScore++;
+        computerScoreValue++;
+        return 'computer';
     }
 }
-
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    console.log(humanScore > computerScore ? "human wins!" : "computer wins!");
-}
-
-playGame();
